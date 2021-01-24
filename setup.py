bf376915385ab41189cf4324b0af4d7cb3d19b55
Anish Lakkapragada <68448458+anish-lakkapragada@users.noreply.github.com>
2021-01-24 11:10:28 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+setup(
+  name = 'sealion',
+  packages = ['sealion'],
+  version = '1.0',
+  license='MIT',
+  description = 'Sealion is a simple machine learning and data science library for beginners and ml-engineers alike.',
+  author = 'Anish Lakkapragada',
+  author_email = 'anish.lakkapragada@gmail.com',
+  url = 'https://github.com/anish-lakkapragada/SeaLion',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/anish-lakkapragada/SeaLion/archive/1.0.tar.gz',
+  keywords = ['Machine Learning', 'Data Science', 'Python'],
+  install_requires=[
+          'sealion',
+          'numpy',
+          'joblib',
+          'pandas',
+          'scipy',
+          'tqdm',
+          'multiprocess'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+  ],
+)
