655406ca5ee36552973340d1f49d9a9428790f91
Anish Lakkapragada <68448458+anish-lakkapragada@users.noreply.github.com>
2021-01-24 10:39:54 -0800

    Create __init__.py

---
+++ sealion/cython_models/__init__.py
@@ -0,0 +1,4 @@
+"""
+Contains all the Cython files that are the backend of the python classes of SeaLion. This dramatically 
+has increased speed. Always call the python classes, but feel free to dig in the .pyx files to see the source!
+"""
