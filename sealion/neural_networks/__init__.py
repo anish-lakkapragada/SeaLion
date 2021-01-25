ef9235723823dee70d2fe5e90ec63f7d53872235
Anish Lakkapragada <68448458+anish-lakkapragada@users.noreply.github.com>
2021-01-24 23:59:54 -0800

    Update __init__.py

--- sealion/neural_networks/__init__.py
+++ sealion/neural_networks/__init__.py
@@ -1,3 +1,7 @@
+"""
+This is the neural networks subpackage of SeaLion for training deep neural networks. Enjoy!
+"""
+
 from sealion.neural_networks.optimizers import GD, Momentum, SGD, AdaGrad, RMSProp, Adam
 from sealion.neural_networks.layers import Dense, Flatten, Dropout, ReLU, ELU, SELU, LeakyReLU, Sigmoid, Tanh, Swish
 from sealion.neural_networks.models import NeuralNetwork, NeuralNetwork_MapReduce
