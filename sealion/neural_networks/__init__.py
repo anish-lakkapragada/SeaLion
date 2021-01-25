35875905b704b375ee882436ed275bec4523b02b
Anish Lakkapragada <68448458+anish-lakkapragada@users.noreply.github.com>
2021-01-24 23:24:56 -0800

    Delete sealion directory

--- sealion/neural_networks/__init__.py
+++
@@ -1,4 +0,0 @@
-from sealion.neural_networks.optimizers import GD, Momentum, SGD, AdaGrad, RMSProp, Adam
-from sealion.neural_networks.layers import Dense, Flatten, Dropout, ReLU, ELU, SELU, LeakyReLU, Sigmoid, Tanh, Swish
-from sealion.neural_networks.models import NeuralNetwork, NeuralNetwork_MapReduce
-from sealion.neural_networks.loss import CrossEntropy, MSE
