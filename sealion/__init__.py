f4a63aa46d6719a9741bd062b9fa6b82f58245ec
Anish Lakkapragada <68448458+anish-lakkapragada@users.noreply.github.com>
2021-01-24 18:49:10 -0800

    Update __init__.py

--- sealion/__init__.py
+++ sealion/__init__.py
@@ -4,12 +4,11 @@ SeaLion is a simple machine learning and data science library.
 =============================================================
 
 Designed with beginners in mind, it has rich documentation via Pydoc and algorithms that span from the most basic
-to more modern approaches. It is meant to help beginners navigate it all, and the documentation not only explains the models
-and their respective functions but also what they are and when to use them. Emphasis was also put on creating
+to more modern approaches. It is meant to help beginners navigate it all, and the documentation not only explains t
+he models and their respective functions but also what they are and when to use them. Emphasis was also put on creating
 new functions to make it interesting for those who are just getting started and seasoned ml-engineers alike.
 
-I hope you enjoy it as much as I did writing it! Please contribute or send feedback to anish.lakkapragada@gmail.com! 
-
+I hope you enjoy it!
 - Anish Lakkapragada 2021
 """
 
@@ -24,5 +23,7 @@ from sealion.neural_networks.optimizers import GD, Momentum, SGD, AdaGrad, RMSPr
 from sealion.neural_networks.layers import Dense, Flatten, Dropout, ReLU, ELU, SELU, LeakyReLU, Sigmoid, Tanh, Swish
 from sealion.neural_networks.models import NeuralNetwork, NeuralNetwork_MapReduce
 from sealion.neural_networks.loss import CrossEntropy, MSE
+
+from sealion import cython_models
 
 __all__ = ['neural_networks', 'decision_trees', 'DimensionalityReduction', 'ensemble_learning', 'regression', 'unsupervised_clustering', 'utils', 'nearest_neighbors', 'cython_models']
