915802dc7b862cc5c7ff7a723ec9a4194e591130
Anish Lakkapragada <68448458+anish-lakkapragada@users.noreply.github.com>
2021-01-26 20:25:55 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'sealion',
   packages = ['sealion'],
-  version = '2.7.7',
+  version = '2.7.8',
   license='MIT',
   description = 'SeaLion is a simple machine learning and data science library for beginners and ml-engineers alike.',
   author = 'Anish Lakkapragada',
   author_email = 'anish.lakkapragada@gmail.com',
   url = 'https://github.com/anish-lakkapragada/SeaLion',
-  download_url = 'https://github.com/anish-lakkapragada/SeaLion/archive/v2.7.7.tar.gz',
+  download_url = 'https://github.com/anish-lakkapragada/SeaLion/archive/v2.7.8.tar.gz',
   keywords = ['Machine Learning', 'Data Science', 'Python'],
   install_requires=[
           'numpy',
