b98463409ead3fd38f8c30d8e8c8987895869ad3
Anish Lakkapragada <68448458+anish-lakkapragada@users.noreply.github.com>
2021-01-26 19:17:53 -0800

    Delete sealion directory

--- sealion/neural_networks/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-This is the neural networks subpackage of SeaLion for building, training, and using deep neural networks. Enjoy!
-"""
-
-from . import layers
-from . import models
-from . import optimizers
-from . import utils
