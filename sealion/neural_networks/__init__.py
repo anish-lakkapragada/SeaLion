0eb38ee12c80505ed56f35a64222f828fbc3d879
Anish Lakkapragada <68448458+anish-lakkapragada@users.noreply.github.com>
2021-01-29 15:41:57 -0800

    Add files via upload

---
+++ sealion/neural_networks/__init__.py
@@ -0,0 +1,9 @@
+"""
+This is the neural networks subpackage of SeaLion for building, training, and using deep neural networks. Enjoy!
+"""
+
+from . import layers
+from . import loss
+from . import models
+from . import optimizers
+from . import utils_nn
