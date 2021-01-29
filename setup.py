a1261ed11133553d80ce671fa90e5919897921d1
Anish Lakkapragada <68448458+anish-lakkapragada@users.noreply.github.com>
2021-01-28 23:55:05 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 import setuptools
 from distutils.core import setup
 
+non_python_files = ['cython_ensemble_learning.cpython-38-darwin.so', 'cython_decision_tree_functions.cpython-38-darwin.so', 'cython_naive_bayes.c', 'cython_decision_tree_functions.pyx', 'cython_tsne.pyx', 'cython_unsupervised_clustering.cpython-38-darwin.so', '.DS_Store', 'cython_decision_tree_functions.so', 'cython_unsupervised_clustering.pyx', 'cython_naive_bayes.so', 'cython_tsne.o', 'cython_knn.so', 'cython_knn.c', 'cython_naive_bayes.pyx', 'cython_ensemble_learning.o', 'cython_decision_tree_functions.o', 'cython_ensemble_learning.pyx', 'cython_naive_bayes.o', 'cython_ensemble_learning.so', 'cython_knn.cpython-38-darwin.so', 'cython_tsne.cpython-38-darwin.so', 'cython_tsne.c', 'cython_naive_bayes.cpython-38-darwin.so', 'cython_knn.pyx', 'cython_knn.o', 'cython_tsne.so', 'cython_ensemble_learning.c', 'cython_decision_tree_functions.c']
+
 setup(
   name = 'sealion',
   packages = setuptools.find_packages(),
-  include_package_data=True,
+  package_data = {'' : non_python_files}, 
   version = '2.8.6',
   license='MIT',
   description = 'SeaLion is a simple machine learning and data science library for beginners and ml-engineers alike.',
