313b48fa3bc0b4b1ef32f2acb990182bf057738e
Anish <68448458+anish-lakkapragada@users.noreply.github.com>
2021-03-04 20:46:41 -0800

    Update __init__.py

--- sealion/__init__.py
+++ sealion/__init__.py
@@ -36,7 +36,7 @@ org_dir = os.getcwd()
 dir_path = os.path.dirname(os.path.realpath(__file__))
 os.chdir(dir_path)
 
-VERSION_NUMBER = "4.2.5"
+VERSION_NUMBER = "4.2.8"
 PYTHON_RUNNING_VERSION = str(sys.version_info.major) + "." + str(sys.version_info.minor)
 
 
